98360472cbfb48297b84b9062699c4f358037484
Jim Mainprice <mainprice@gmail.com>
2019-12-13 18:02:29 +0100

    Simplify again

--- tests/utests.cmake
+++ tests/utests.cmake
@@ -1,31 +1,11 @@
-find_package(Threads REQUIRED)
-
-# Enable ExternalProject CMake module
-include(ExternalProject)
-
-# Download and install GoogleTest
-ExternalProject_Add(
-    gtest
-    URL https://github.com/google/googletest/archive/master.zip
-    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/gtest
-    # Disable install step
-    INSTALL_COMMAND ""
-)
-
-# Get GTest source and binary directories from CMake project
-ExternalProject_Get_Property(gtest source_dir binary_dir)
-
-# Create a libgtest target to be used as a dependency by test programs
-add_library(libgtest IMPORTED STATIC GLOBAL)
-add_dependencies(libgtest gtest)
-set_target_properties(libgtest PROPERTIES
-    "IMPORTED_LOCATION" "${binary_dir}/lib/libgtest.a"
-    "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
-)
-include_directories("${source_dir}/googletest/include")
-
+find_package(gtest)
 enable_testing()
-include_directories(${GTEST_INCLUDE_DIRS})
+if(${GTEST_FOUND})
+    message(${GTEST_INCLUDE_DIRS})
+    include_directories(${GTEST_INCLUDE_DIRS})
+else()
+    message("Not Found")
+endif()
 add_executable(atomic_operators_utest tests/atomic_operators_utest.cpp)
 target_link_libraries(atomic_operators_utest ${GTEST_LIBRARY} ${PROJECT_NAME})
 include(GoogleTest)
