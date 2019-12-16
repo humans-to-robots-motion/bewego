4eda6f354d3821bb9e39687822b1654958626024
Jim Mainprice <mainprice@gmail.com>
2019-12-16 17:52:48 +0100

    Add ifopt test

--- tests/utests.cmake
+++ tests/utests.cmake
@@ -11,3 +11,14 @@ add_executable(atomic_operators_utest tests/atomic_operators_utest.cpp)
 target_link_libraries(atomic_operators_utest ${GTEST_LIBRARY} ${PROJECT_NAME})
 include(GoogleTest)
 gtest_add_tests(TARGET atomic_operators_utest)
+
+find_package(ifopt)
+if(ifopt_FOUND)
+    # To install on mac 
+    #  - https://github.com/ethz-adrl/ifopt
+    #  - brew install ipopt
+    # Formulate (ifopt:ifopt_core) and solve (ifopt::ifopt_ipopt) the problem
+    add_executable(ifopt_test tests/ifopt_test.cpp)
+    # Pull in include directories, libraries, ... 
+    target_link_libraries(ifopt_test PUBLIC ifopt::ifopt_ipopt) 
+endif() 
