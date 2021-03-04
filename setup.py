d31bc9f895295dfa3a15e9bdc9c8d89406a3df00
anindex <an.thai.le97@gmail.com>
2021-03-04 19:53:04 +0100

    Add pybind module in setup
    Signed-off-by: anindex <an.thai.le97@gmail.com>

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup( 
-    packages=['bewego'],
-    package_dir={'': '.'}
+    packages=['pybewego'],
+    package_dir={'': '.'},
+    package_data={'pybewego': ['_pybewego.cpython-38-x86_64-linux-gnu.so']},
+    include_package_data=True
 )
 
 setup(**d)
