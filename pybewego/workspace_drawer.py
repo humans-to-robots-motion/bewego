720ffaf940418ca365be776750e239b6d05eaf22
Jim Mainprice <mainprice@gmail.com>
2021-03-14 11:37:00 +0100

    start working on socket connection to drawer

---
+++ pybewego/workspace_drawer.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2021, University of Stuttgart
+# All rights reserved.
+#
+# Permission to use, copy, modify, and distribute this software for any purpose
+# with or without   fee is hereby granted, provided   that the above  copyright
+# notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+# REGARD TO THIS  SOFTWARE INCLUDING ALL  IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS. IN NO EVENT SHALL THE AUTHOR  BE LIABLE FOR ANY SPECIAL, DIRECT,
+# INDIRECT, OR CONSEQUENTIAL DAMAGES OR  ANY DAMAGES WHATSOEVER RESULTING  FROM
+# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+# OTHER TORTIOUS ACTION,   ARISING OUT OF OR IN    CONNECTION WITH THE USE   OR
+# PERFORMANCE OF THIS SOFTWARE.
+#
+#                                        Jim Mainprice on Sunday March 14 2021
+
+
+import socket
+import sys
+
+import argparse
+import zmq
+
+parser = argparse.ArgumentParser(description='zeromq server/client')
+parser.add_argument('--bar')
+args = parser.parse_args()
+
+if args.bar:
+    # client
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect('tcp://127.0.0.1:5555')
+    socket.send(args.bar.encode('ascii'))
+    msg = socket.recv()
+    print(msg)
+else:
+    # server
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind('tcp://127.0.0.1:5555')
+    while True:
+        msg = socket.recv()
+        print(msg)
+        if msg == 'zeromq':
+            socket.send('ah ha!'.encode('ascii'))
+        else:
+            socket.send('...nah'.encode('ascii'))
