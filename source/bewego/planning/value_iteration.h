0b4b4f1143e6404c808bff19ccc68e82e00cbdda
Jim Mainprice <mainprice@gmail.com>
2021-07-03 15:34:19 +0200

    Start thinking about state visitation frequencies

--- source/bewego/planning/value_iteration.h
+++ source/bewego/planning/value_iteration.h
@@ -29,6 +29,18 @@
 
 namespace bewego {
 
+/**
+ * \brief Value Iteration on costmaps
+ *        The planning problem is assumed to be 8 connected
+ *        We condier cost instead of reward, the aglorithm is is
+ *        is sumarized bellow:
+ *
+ *    repeat
+ *      k ←k+1
+ *      for each state s do
+ *          Vk[s] = min_a ∑_s' P(s'|s,a) (C(s,a,s')+ γVk-1[s'])
+ *    until ∀s |Vk[s]-Vk-1[s]| < θ
+ */
 class ValueIteration {
  public:
   ValueIteration()
@@ -54,6 +66,29 @@ class ValueIteration {
   double max_iterations_;
   double max_value_;
   double gamma_;  // discount factor
+};
+
+/**
+ * \brief Soft Q Iteration on costmaps
+ *        here the underlying assumtion is that we have a softmin policy
+ */
+class SoftQIteration {
+ public:
+  SoftQIteration()
+      : theta_(1e-6),
+        max_iterations_(1e+5),
+        // max_value_(100000),
+        max_value_(std::numeric_limits<double>::max()),
+        gamma_(1.) {}
+};
+
+/**
+ * \brief State Vistation Frequency on costmaps
+ *        here the underlying assumtion is that we have a softmin policy
+ */
+class StateVistationFrequency {
+ public:
+  StateVistationFrequency() {}
 };
 
 }  // namespace bewego
